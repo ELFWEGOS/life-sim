8d4dec5ddbfcc8af52a556d2745d0021db222687
ELFWEGOS <mizanmazin49@gmail.com>
2026-03-26 16:00:29 +0100

    reorganizing the main while loop beacause of the new renderer file & package

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,21 @@
+import Rendering.Renderer;
 import Simulation.Simulation;
+import World.Map;
 
 import java.io.IOException;
 
 
 public class Test {
     public static void main(String[] args) throws InterruptedException, IOException {
-
-        Simulation simulation = new Simulation();
+        Map map = new Map(10,10);
+        Simulation simulation = new Simulation(map);
+        Renderer renderer = new Renderer(map);
         simulation.generateMap(4,1);
 
         while (true){
             simulation.update();
-            simulation.renderMap();
-            Thread.sleep(100);
+            renderer.renderMap(simulation);
+            Thread.sleep(20);
         }
 
     }
